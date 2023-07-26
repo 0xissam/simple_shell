00665f52807c1d3559b46d519bbbf12560b587ad
0xissam <hah305581@gmail.com>
2023-07-26 11:58:26 +0100

    update

---
+++ build.c
@@ -0,0 +1,25 @@
+/**
+ * b_cd - cd
+ * @d: data structinput
+ * Return: nothing
+ */
+void b_cd(data *d)
+{
+	char *dir = d->av[1];
+	char cwd[256];
+
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
+		perror("getcwd() error");
+	_setenv(d, "PWD", cwd);
+	if (!dir)
+		dir = _getenvirement("HOME");
+	if (chdir(dir) == -1)
+		perror("cd");
+	else
+	{
+		_setenv(d, "OLDPWD", _getenvirement("PWD"));
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			perror("getcwd() error");
+		_setenv(d, "PWD", cwd);
+	}
+}
--- builtin_funcs.c
+++ builtin_funcs.c
@@ -88,31 +88,6 @@ void b_unsetenv(data *d)
 				environ[j] = environ[j + 1];
 }
 
-/**
- * b_cd - cd
- * @d: data structinput
- * Return: nothing
- */
-void b_cd(data *d)
-{
-	char *dir = d->av[1];
-	char cwd[256];
-
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-		perror("getcwd() error");
-	_setenv(d, "PWD", cwd);
-	if (!dir)
-		dir = _getenvirement("HOME");
-	if (chdir(dir) == -1)
-		perror("cd");
-	else
-	{
-		_setenv(d, "OLDPWD", _getenvirement("PWD"));
-		if (getcwd(cwd, sizeof(cwd)) == NULL)
-			perror("getcwd() error");
-		_setenv(d, "PWD", cwd);
-	}
-}
 
 /**
  * b_exit - exit sh shell
